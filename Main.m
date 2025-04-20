28f02db95a368d40dbcfae164bcf3d3ad33ffc30
Slugbotics <slugbots@ucsc.edu>
2025-04-20 03:08:23 -0700

    Updated

--- Main.m
+++ Main.m
@@ -1,7 +1,18 @@
-data = Database(44);
+data = Database(22);
 t = data.time_inserted; 
+latitudes = p_lat;
+longitudes = p_lon;
+height_agl = a_agl;
+
 
 MapPos();
+
+addpath(genpath('../FAAObjects'));
+addpath(genpath('../FAAObjects/DOF_250119'));
+object_data_parser(latitudes, longitudes, height_agl);
+
+%{
+
 Fuel();
 Thrust();
 
@@ -9,7 +20,6 @@ Thrust();
 %addpath(genpath('./TerrainData/'));
 %test();
 
-%{
 SpeedAltitudePlots();
 
 Thrust();
